ec97bbc07b62bb3871434966acb2ed254cd82e0b
ali <aliakbar.mehdizadeh@gmail.com>
2023-06-29 18:28:06 -0700

    app done

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import BlogPost
 # Create your views here.
 
-def blog_post_detail_post(request):
+def blog_post_detail(request):
     obj = BlogPost.objects.get(id=1)
-    template_name = 'blog_post_detail'
+    template_name = 'blog_post_detail.html'
     context = {'object': obj}
     return render( request, template_name, context)
--- try_django/try_django/urls.py
+++ try_django/try_django/urls.py
@@ -15,19 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from blog.views import blog_post_detail_post
+from blog.views import blog_post_detail
 
 from .views import (
     home_page,
     contact_page,
-    about_page
+    about_page,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home_page),
     path('about/', about_page),
-    path('contact/', contact_page)
-#    path('blog/', blog_post_detail_post)
+    path('contact/', contact_page),
+    path('blog/', blog_post_detail)
 ]
