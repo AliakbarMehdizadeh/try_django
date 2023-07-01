edd41be4dcb64495b02d22b4d19f47597f24ab58
ali <aliakbar.mehdizadeh@gmail.com>
2023-06-30 20:38:18 -0700

    done with CRUD

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -3,11 +3,31 @@ from .models import BlogPost
 from django.http import Http404
 # Create your views here.
 
-def blog_post_detail(request, slug):
-    try:
-        obj = BlogPost.objects.get(slug=slug)
-    except:
-        raise Http404
+def blog_post_list_view(request):
+    qs = BlogPost.objects.all()
+    template_name = 'blog_post_list.html'
+    context = { 'object_list': qs }
+    return render(request, template_name, context)
+
+def blog_post_create_view(request):
+    template_name = 'blog_post_create.html'
+    context = { 'form': None}
+    return render(request, template_name, context)
+
+def blog_post_detail_view(request,slug):
+    obj = BlogPost.objects.get(slug=slug)
     template_name = 'blog_post_detail.html'
+    context = {'object': obj}
+    return render( request, template_name, context)
+
+def blog_post_update_view(request, slug):
+    obj = BlogPost.objects.get(slug=slug)
+    template_name = 'blog_post_update_view.html'
+    context = {'object': obj}
+    return render( request, template_name, context)
+
+def blog_post_delete_view(request,slug):
+    obj = BlogPost.objects.get(slug=slug)
+    template_name = 'blog_post_delete_view.html'
     context = {'object': obj}
     return render( request, template_name, context)
--- try_django/try_django/urls.py
+++ try_django/try_django/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import blog_post_detail
+from django.urls import path, include
+from blog.views import (
+    blog_post_create_view,
+)
 
 from .views import (
     home_page,
@@ -28,5 +30,6 @@ urlpatterns = [
     path('home/', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
-    path('blog/<str:slug>/', blog_post_detail)
+    path('blog-new/', blog_post_create_view),
+    path('blog/', include('blog.urls'))
 ]
